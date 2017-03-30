e55f36756e059b3424f1549b4a51e8aca602130f
Marcus Henry, Jr <marcus@dreadedsoftware.com>
2017-03-29 20:18:16 -0400

    Filter type class

---
+++ src/main/scala/justf/Filter.scala
@@ -0,0 +1,18 @@
+package justf
+
+/**
+ * defines a filter over an F. Given a function A => Boolean
+ * returns a function which will accept an F[A] and
+ * return a filtered F[A].
+ * @tparam F the container type to filter over.
+ */
+trait Filter[F[_]]{
+  /**
+   * performs the filter operation
+   * @tparam A the type within the container
+   * @param f The filter function
+   * @return A function F[A] => F[A] which represents
+   * a filter operation for F with type A.
+   */
+  def filter[A](f: A => Boolean): F[A] => F[A]
+}
